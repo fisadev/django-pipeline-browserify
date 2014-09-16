5be0ed305c34de63b8a3ef9d46251f1cc343651e
Juan Pedro Fisanotti <fisadev@gmail.com>
2014-09-16 12:01:42 -0300

    Use original file name for file-based parameters

--- pipeline_browserify/compiler.py
+++ pipeline_browserify/compiler.py
@@ -7,18 +7,19 @@ class BrowserifyCompiler(SubProcessCompiler):
     output_extension = 'browserified.js'
 
     def match_file(self, path):
+        self.original_path = path
         return path.endswith('.browserify.js')
 
     def compile_file(self, infile, outfile, outdated=False, force=False):
         file_args = getattr(settings, 'PIPELINE_BROWSERIFY_FILE_ARGUMENTS', {})
         global_args = getattr(settings, 'PIPELINE_BROWSERIFY_ARGUMENTS', '')
 
-        args = global_args + ' ' + file_args.get(infile, '')
+        args = global_args + ' ' + file_args.get(self.original_path, '')
 
-        command = "%s %s %s > %s" % (
-            getattr(settings, 'PIPELINE_BROWSERIFY_BINARY', '/usr/bin/env browserify'),
-            args,
-            infile,
-            outfile
-        )
+        command = "%s %s %s > %s" % (getattr(settings,
+                                             'PIPELINE_BROWSERIFY_BINARY',
+                                             '/usr/bin/env browserify'),
+                                     args,
+                                     infile,
+                                     outfile)
         return self.execute_command(command, cwd=dirname(infile))
